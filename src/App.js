9d97d68babddffb7a22d663b5936af1e4f43beae
mrfortune <rharris@worldshaker.com>
2022-06-28 21:42:17 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Button from "@mui/material/Button";
+import { ResponsiveAppBar } from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <Button variant="contained">Hello World</Button>
+      <ResponsiveAppBar> </ResponsiveAppBar>
     </div>
   );
 }
