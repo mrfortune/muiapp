9da5c81543d39d6cbf876677f2b93db4cd663d1d
mrfortune <rharris@worldshaker.com>
2022-06-28 20:51:30 -0400

    commit

---
+++ src/components/about.jsx
@@ -0,0 +1,48 @@
+
+
+<main class="flex-shrink-0 text-dark">
+	<section>
+<div class="container">
+	<div class="row">
+		<div class="co-7">
+
+		</div>
+		<div class="col-5">
+			<h1 class="mt-5 mb-4 display-1">The Story</h1>
+		</div>
+	</div>
+	<div class="row">
+		
+		<div class="col-7">
+				 <article class="text">
+ 
+<p>I am <strong>Robert Harris</strong>, and I am a <strong>UX Engineer, Designer, Researcher, and Architect</strong>. I have worked with large brands successfully delivering digital products and solutions for their customers and business. I collaborate with cross-functional teams to deliver these solutions.</p>	 
+<p class="mt-3">My love for design began as a child where I would sketch for hours, and I developed a love for drawing characters and creating figures with clay. I was always trying to create something new and interesting. That creative interest evolved into music, video and photography, so when the Web became ubiquitous it was obvious how tech, business and creative work would merge into an interactive experience. I did not realize it at the time but that was my first insight into the world of user experience.</p>
+<p class="mt-3">My degree in business trained me to think about marketing, management, statistics, and the challenges involved with succeeding in business. I learned to develop websites, and then I learned online video production, photography and how to translate designs into code. At Showtime I was tasked with editing clips weekly for our series shows using Final Cut Pro and other video processing/streaming  tools. I have also taken a course in editing using Premier. But, with these skills, I would always find myself asking “why?”. UX Design methodologies, skills, thinking, language and processes has provided me with the approach to get those answers and create greater questions.</p>
+<p class="mt-3"><strong>WorldShaker Interactive</strong> was founded to solve a problem that my cousin and I had identified, wrote a complete business plan for and designed a mock-up. Our plan did not get funded but I see it everytime I visit SoundCloud. I'm not saying that they stole our plan, but their business model for artists is basically what we had developed. Glad to see that our idea proved to be a solution for many independent artists and other users.</p>
+<p class="mt-2">I am still designing soltuons to problems and who knows may come up with another idea that can shake the world. But, for now, I am happy to share my skills with companies small, medium and large to help them deliver better user experiences to their customers.</p>
+</p>  
+ 
+</article>				
+		   </div> 
+		   
+		   <div class="col-5">
+			<div class="myimagebox">
+				<img src="img/mebwsmiling.jpg" class="img-fluid rounded">
+				<p class="mb-3 mt-3 text-center">Robert S. Harris, <strong><i>UX Engineer, Designer, Researcher, and Architect</i></strong>
+			<a href="RobertSHarris.pdf" target="_blank"><br><i class="bi bi-cloud-arrow-down">&nbsp;</i>Download Resume...</a></p>
+	
+		</div>
+		
+</div>		   
+
+</div>
+		
+		  
+					  
+				   </div>
+</div>
+	</section>				
+</main>
+		
+			
---
+++ src/components/wsgr.jsx
@@ -0,0 +1,337 @@
+
+<main class="flex-shrink-0 text-dark">
+	<section>
+<div class="container px-0">
+		<div class="row">
+<div class="col-8">
+	<div class="wsgrherohome">
+					<div class="hero-overlay">
+						<h1 class="mb-2">Improved Dog Rescue and Adoption User Experience.</h1>
+					<h3>Redesign of West Side German Shepherd Rescue's website better user experience.</h3>
+					</div>
+					
+	</div>
+<div class="col-4">
+
+</div>
+		</div>
+				
+<img src="img/wsgrherohome.jpg"	class="img-fluid">
+</div>
+	</div>
+	</div>
+	</section>
+<section>
+<div class="container-md mt-3 px-0">
+		<div class="row">
+			
+		
+		<div class="col-md-4">
+<h5 class="mb-3">Overview</h5>
+					<p>West Side German Shepherd Rescue's website needs a complete overhaul and look and feel to
+						deliver a better user experience for it's customers.
+					</p>
+		</div>
+					
+<div class="col-md-4">
+					<h5 class="mb-3">The Team &amp; Timeframe</h5>
+					<p>The project time frame was a 2 week sprint.
+					</p>
+					<ul>
+						<li>Madeline Lipkin, Lead UX Researcher</li>
+						<li>Robert Harris, Lead Information Architect</li>
+						<li>Belinda Miller, Lead Visual Designer</li>
+					</ul>
+
+
+				</div>
+				<div class="col-md-4">
+					<h5 class="mb-3">Tools &amp; Methods Used</h5>
+					<ul><li>Affinity Mapping</li>
+					<li>User Interviews</li>
+					<li>User Surveys</li>
+					<li>Competitive &amp; Comparative Analysis</li>
+					<li> Miro</li>
+					<li> Figma</li>
+					<li>Adobe CC</li>
+					<li>Usability Test</li>
+					</ul>
+
+				</div>
+			
+	
+</div>
+</div>
+</section>
+			
+<section>
+	<div class="container-md px-0">
+		<div class="row">
+		<div class="col-xl-4"></div>	
+				<div class="col-xl-4">
+					<h5 class="mt-5 mb-3">Problem</h5>
+				
+						<h2 class="mb-2">The Experience Should be Personable</h2>
+					
+
+
+					<p>
+						Users need a personable experience to select an animal rescue in order to find a dog to add to their family. The user who will be going to WSGR's site  to search for and adopt a dog needs to feel a level of trust and personal touch on website. At the moment the current site is very old and chaotic and it is not serving WSGR well as far as the image.
+					</p>
+
+				</div>
+				<div class="col-xl-4"></div>	
+			</div>
+	</div>
+
+</section>
+<section>
+<div class="container-md px-0">
+<div class="row">
+	<div class="col-xl-4"></div>		
+				<div class="col-xl-4">
+					<h5 class="mt-5 mb-3">Solution Overview</h5>
+					
+						<h2 class="mb-2">How might we...?</h2>
+					
+					<p class="mb-5"><b>How might we design an adoption website for West Side German Shepherd Rescue's
+						customer?</b> The challenge was to design and build a user-centered website for West Side German Shepherd
+						Rescue that organized the wines in proper categories and provided navigation and search for
+						the user to be able to easily find and purchase wines and have them delivered.
+					</p>
+					<div class="row">
+						<div class="col">
+							<img src="img/wsgrba.png" class="img-fluid border rounded">
+						</div>
+						
+					</div>
+				</div>
+				<div class="col-xl-4"></div>	
+				</div>
+</div>
+
+	
+</section>		
+			
+	<section>
+		<div class="container-md px-0">
+<div class="row">
+	<div class="col-xl-4"></div>	
+				<div class="col-xl-4">
+					<h5 class="mt-5 mb-3">Approach</h5>
+					
+						<h2 class="mb-2">Seek Users Who Have or Are Adopting Dogs
+						</h2>
+					<p>Our approach to this project was to seek out users who have adopted dogs or are currently adopting dogs and ask them questions about their experience and sites that they used. We also looked at other adoption sites to get an idea of how the competitors sites look.</p>
+
+
+				</div>
+				<div class="col-xl-4"></div>	
+			</div>
+		</div>
+
+	</section>		
+		
+	<section>
+		<div class="container-md px-0">
+			<div class="row">
+				<div class="col-xl-4"></div>	
+				<div class="col-xl-4">
+					<h5 class="mt-5 mb-3">Activities & Deliverables</h5>
+				
+						<h2 class="mb-2">Hours of Reasearch, Architecture and Design</h2>
+					
+					<p>West Side German Shepherd Rescue's new website solution will provide its customers with the
+						ability to search for a dog to adopt and submit the adoption application.</p>
+
+<h3 class="mt-4 mb-3">Research</h3>
+<h4 class="mb-3">Persona</h4>
+					
+						<h2 class="mb-2">Dog Lover Looking to Adopt a German Shepherd</h2>
+						<p class="mb-5">Our persona - Carol Olsen - is the summary of the users that we researched who use websites to adopt dogs and plan to or have adopted dogs in the past.</p>
+						<div class="col-lg-12 mb-5">
+							<img src="img/wsgrpersona.jpg" class="img-fluid border rounded">
+						</div>
+					
+						<h4>1. User Interviews</h4>
+						<p>By observing the participants within the context of buying wine at an, and during, I was able
+							to question and observe how they order wine and why they use the method that they use -
+							mobile phone. As people arrived over the 5 or 6 hours that we were gathered we talked about
+							their favorite local wine stores and their sites and apps.</p>
+							<h6>Interview Questions:</h6>
+					<ul>
+						<li>1. Tell me about your experience of finding and adopting your dog online.<ul>
+								<li>a. Website used</li>
+								<li>b. Timeline (from first search to adoption)</li>
+								<li>c. Adoption application process</li>
+								<li>d. Retrieving dog</li>
+							</ul>
+						</li>
+						<li>What drew you to your dog specifically?</li>
+						<li>Did you have any special considerations when searching for your dog? (e.g. good with
+							cats/other dogs/kids, male/female, age, housebroken)</li>
+						<li>Why did you use (website they answered in survey) over other websites?<ul>
+								<li>a. Was there anything that stood out to you as something the site did well?</li>
+								<li>b. Was there anything that stood out to you as something the site did not do
+									well?</li>
+							</ul>
+						</li>
+						<li>If you were to looking to adopt another dog, would you use this website again? Why?
+						</li>
+						<li>Have you ever volunteered with or donated to a shelter or rescue? How was that
+							experience?</li>
+						<li>Have you ever/would you ever consider hosting a foster dog? Elaborate.</li>
+					</ul>
+					<h4>2.User Survey </h4>
+					<p>We created a survey and solicited parcipants to participate via social media platforms.</p>
+					<h6>Data Gathered</h6>
+					<ul>
+						<li>Accounts set up with their favorite local wine stores</li>
+						<li>Delivery was preffered method of getting wine</li>
+						<li>Site content was organized and searchable for users/customers</li>
+
+					</ul>
+					<h4>3. Affinity Mapping & Data Synthesis</h4>
+					<p>By observing the participants within the context of buying wine at an, and during, I was able
+						to question and observe how they order wine and why they use the method that they use -
+						mobile phone. As people arrived over the 5 or 6 hours that we were gathered we talked about
+						their favorite local wine stores and their sites and apps.</p>
+					<h6>Data Gathered:</h6>
+					<div class="col-lg-12 mt-5 mb-5">
+						<img src="img/Affinity Map.jpeg" class="img-fluid border rounded">
+					</div>
+					<h4 class="mb-3">2. Competitive Analysis</h4>
+					
+<h2 class="mb-2">Competitors Have Great Looking Websites</h2>
+<p>A quick search for dog rescue sites and one can see the stark difference between sheprescue.org and other dog rescue sites. We looked at a few but for the study I have only included two.</p>
+					
+<div class="col-lg-12 d-flex text-center mt-5">
+	<div style="margin:0 auto;">
+		<img src="img/Competitive-Analysis-1.png" class="img-fluid border rounded">
+	</div>			
+	
+		
+	</div>
+
+	<div class="col-lg-12 text-center d-flex mt-5 align-items-center">
+		<div style="margin:0 auto;">		
+		<img src="img/Competitive-Analysis-2.png" class="img-fluid border rounded">
+			</div>
+		</div>
+
+</div>
+					<div class="col">
+
+				</div>
+					</div>
+				
+			</div>
+
+		</div>
+
+	</section>
+
+
+<section>
+<div class="container-md px-0">
+<div class="row">
+	<div class="col-xl-4">
+
+	</div>
+				<div class="col-xl-4">
+					<h5 class="mt-5 mb-3">Design</h5>
+					
+<h2 class="mb-2">Design for Search, Dog Bio and Adoption Application</h2>
+
+
+					
+				<h4>Site Maps</h4>	
+				<p>Here is the new information architecture that we came up with based on the insights gathered. We eliminated a few pages that made no sense and consolidated that information into other pages.</p>
+				<div class="col-lg-12 d-flex text-center align-items-center">
+					<img src="img/Sitemap - mobile v3.jpeg" class="img-fluid mt-5 mb-5 border rounded">
+				</div>
+				<div class="col-lg-12 text-center mb-5 d-flex align-items-center">
+					<img src="img/Sitemap - desktop v2.jpeg" class="img-fluid border rounded">
+				</div>
+				<h4>User Flow</h4>
+				<p>We created a user flow that expects to be able to search and/or go directly to the wine
+					varietals, regions and coutries that the customer is looking for. If a user is signed up
+					he/she can see the wines that they bought and repurchase it/them. </p>
+				<p>No matter what choice the user makes, they will be able now check out and complete their
+					purchase online and select delivery to a selected address.</p>
+					<div class="col-lg-12 mt-5">
+			
+						<img src="img/User Flow.jpeg" class="img-fluid border rounded">
+					</div>
+					
+			</div>
+<div class="col-xl-4">
+
+</div>
+			</div>
+
+</div>
+
+</section>
+<section>
+	<div class="container-md px-0">
+		<div class="row">
+			<div class="col-xl-4">
+
+			</div>
+			<div class="col-xl-4">
+				<h5 class="mt-5 mb-3">Visual Design</h5>
+				<h2 class="mb-2">Let's Get Some Wireframes Done</h2>
+				<p>The visual design incorporates all of the UI elements and functionality found in other
+						e-commerce wine sites. We have incorporated search and faceted search to help users select
+						wine by ratings, price, region and country.</p>
+				<div class="col-lg-12 mt-5">
+				
+					
+					
+					<img src="img/wsgrwires.png" class="img-fluid border rounded mb-5">
+				</div>
+				<h2 class="mb-2">Easily Find the Dog You Like and Apply to Adopt</h2>
+				<p>We began this process with some low-fidelity sketches and wireframes and we quickly turned those in mid-fidelity wireframes.
+					We began by ddesigning the mobile experience first and then designed the desktop expeience once we settled on the direction that we wanted to go in.</p>
+					<div class="col-lg-12 mt-5">
+						<h4 class="mt-5">Mobile Version</h4>
+						<img src="img/wsgrmobileshots.png" class="img-fluid mt-5 mb-5 border rounded">
+						<h4 class="mt-5">Desktop Version</h4>
+						<img src="img/wsgr-desktop.jpg" class="img-fluid border mt-5 mb-5 rounded">
+					</div>
+			</div>
+			<div class="col-xl-4">
+
+
+			</div>
+		</div>
+	</div>
+
+
+</section>			
+			
+<section class="pb-5">
+	<div class="container-md px-0">
+<div class="row">
+	<div class="col-xl-4"></div>	
+		<div class="col-xl-4">
+			<h5 class="mt-5 mb-3">Results & Reflections</h5>
+				<h2 class="mb-2">Fantastic Redesign with Future Improvements to Come</h2>
+			<p class="mb-3">With a comeplete redesign of sheprescue.org we believe that we have improved the user experience for their customers. The site is no longer outdated and is better organized. The look and feel is clean and with consisten colors and typography.</p>
+			<p>We will continue working on the design by conducting usability testing.
+			</p>
+
+		</div>
+		<div class="col-xl-4"></div>	
+	</div>
+	</div>
+	
+</section>
+
+
+</div>
+</div>
+			
+</main>
+		
